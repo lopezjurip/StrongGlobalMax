c6f9163884e090c72a304619809d2b970b984e83
Patricio López <lopezjuripatricio@gmail.com>
2015-07-16 09:14:15 -0300

    ruby styleguide

--- code.rb
+++ code.rb
@@ -1,40 +1,43 @@
 class Array
-  def median
-    mid = (self.length - 1) / 2.0
+  def middle
+    mid = (length - 1) / 2.0
     (self[mid.floor] + self[mid.ceil]) / 2.0
   end
 end
 
-def top(array, &block)
-    return array.first if array.length == 1
+def global_max(array, &block)
+  return array.first if array.length == 1
 
-    first = block.call(array.first)
-    last = block.call(array.last)
-    middle = block.call(array.median)
+  first = block.call(array.first)
+  last = block.call(array.last)
+  middle = block.call(array.middle)
 
-    lower = array.slice(0, array.length/2)
-    higher = array.slice(array.length/2, array.length)
+  lower = array.slice(0, array.length / 2)
+  higher = array.slice(array.length / 2, array.length)
 
-    if middle.between?(first, last)
-        return (first < last) ? top(higher, &block) : top(lower, &block)
+  if middle.between?(first, last)
+    if first < last
+      return global_max(higher, &block)
     else
-        return [top(lower, &block), top(higher, &block)].max
+      return global_max(lower, &block)
     end
+  else
+    return [global_max(lower, &block), global_max(higher, &block)].max
+  end
 end
 
-
 arrays = [
-    [1, 2, 4, 5, 10, 7, 5, 2, 1, 0],
-    [100, 90, 80, 10],
-    [0, 0.1, 0.2, 0.3, 0.4, 0.5],
-    [42]
+  [1, 2, 4, 5, 10, 7, 5, 2, 1, 0],  # Expected max: 10
+  [100, 90, 80, 10],  # Expected max: 
+  [0, 0.1, 0.2, 0.3, 0.4, 0.5],
+  [42]
 ]
 
 function = proc do |x|
-    x
+  x
 end
 
 arrays.each do |array|
-    result = top(array, &function)
-    puts result
+  result = global_max(array, &function)
+  puts result
 end
